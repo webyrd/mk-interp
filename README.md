e6a9db1357276a86b5bfd1de03d7e51c2988dd27
William Byrd <webyrd@cs.indiana.edu>
2013-05-14 06:41:28 +0200

    updated comments

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ mk-interp
 =========
 
 miniKanren interpreter in Scheme
+
+Written in the style of Indiana University's C311/B521 courses by Jason Hemann, Dan Friedman, and Will Byrd.
--- mk-interp.scm
+++ mk-interp.scm
@@ -1,4 +1,4 @@
-;;; miniKanren interpreter (with a subset of Scheme as well)
+;;; miniKanren interpreter in Scheme
 ;;;
 ;;; Written in the style of Indiana University's C311/B521 courses
 ;;; by Jason Hemann, Dan Friedman, and Will Byrd
